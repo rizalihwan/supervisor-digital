9c36a0240ed434083d265ea3d0350ba67692119c
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2020-12-06 07:53:55 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,22 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware('auth')->group(function() {
     // GURU
-    Route::group(['middleware' => 'checkRole:guru'], function(){
-        Route::get('/guru_upload', 'GuruController@index')->name('guru.index');
-        Route::get('/jadwal_guru', 'GuruController@jadwal')->name('guru.jadwal');
-        Route::post('/create_uploadguru', 'GuruController@store')->name('guru.store');
-        Route::delete('/delete_uploadguru/{id}', 'GuruController@destroy')->name('guru.destroy');
+    Route::prefix('guru')->middleware('checkRole:guru')->name('guru.')->group(function(){
+        Route::get('/guru_upload', 'GuruController@index')->name('index');
+        Route::get('/jadwal_guru', 'GuruController@jadwal')->name('jadwal');
+        Route::post('/create_uploadguru', 'GuruController@store')->name('store');
+        Route::delete('/delete_uploadguru/{id}', 'GuruController@destroy')->name('destroy');
     });
 
     // SUPERVISOR
-    Route::group(['middleware' => 'checkRole:supervisor'], function(){
-        Route::get('/supervisor_nilai', 'SupervisorController@index')->name('supervisor.index');
-        Route::get('/supervisor_edit/{id}', 'SupervisorController@edit')->name('supervisor.edit');
-        Route::put('/supervisor_update/{id}', 'SupervisorController@update')->name('supervisor.update');
+    Route::prefix('supervisor')->middleware('checkRole:supervisor')->name('supervisor.')->group(function(){
+        Route::get('/supervisor_nilai', 'SupervisorController@index')->name('index');
+        Route::get('/supervisor_edit/{id}', 'SupervisorController@edit')->name('edit');
+        Route::put('/supervisor_update/{id}', 'SupervisorController@update')->name('update');
     });
 
     // KURIKULUM
-    Route::group(['middleware' => 'checkRole:kurikulum'], function(){
+    Route::prefix('kurikulum')->middleware('checkRole:kurikulum')->group(function(){
         Route::get('/pemilihan_supervisor', 'KurikulumController@supervisor')->name('kurikulum.supervisor');
         Route::get('/kurikulum', 'KurikulumController@index')->name('kurikulum.index');
         Route::post('/store_jadwal', 'KurikulumController@store')->name('jadwal.store');
@@ -36,8 +36,8 @@ Route::middleware('auth')->group(function() {
     });
 
     // KEPSEK
-    Route::group(['middleware' => 'checkRole:kepsek'], function(){
-        Route::get('/kepsek', 'KepsekController@index')->name('kepsek.index');
-        Route::get('/laporan_supervisi', 'KepsekController@generate_report')->name('kepsek.laporan');
+    Route::prefix('kepsek')->middleware('checkRole:kepsek')->name('kepsek.')->group(function(){
+        Route::get('/kepsek', 'KepsekController@index')->name('index');
+        Route::get('/laporan_supervisi', 'KepsekController@generate_report')->name('laporan');
     });
 });
